5fa9255ee51357950361c6cce4bc638d84b29266
Lucas <lucas94gv@gmail.com>
2024-02-01 18:11:29 +0100

    migration and update schema of users table

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_129_183_309) do
+ActiveRecord::Schema[7.1].define(version: 20_240_201_125_603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pgcrypto'
   enable_extension 'plpgsql'
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum 'role', %w[employee manager admin superadmin]
 
   create_table 'companies', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
     t.string 'cif', null: false
@@ -23,4 +27,35 @@ ActiveRecord::Schema[7.1].define(version: 20_240_129_183_309) do
     t.datetime 'updated_at', null: false
     t.index ['cif'], name: 'index_companies_on_cif', unique: true
   end
+
+  create_table 'users', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.uuid 'company_id', null: false
+    t.enum 'role', default: 'employee', null: false, enum_type: 'role'
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.integer 'failed_attempts', default: 0, null: false
+    t.string 'unlock_token'
+    t.datetime 'locked_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['company_id'], name: 'index_users_on_company_id'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index ['unlock_token'], name: 'index_users_on_unlock_token', unique: true
+  end
+
+  add_foreign_key 'users', 'companies'
 end
