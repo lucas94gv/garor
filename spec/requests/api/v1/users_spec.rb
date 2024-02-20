3efe68a007b2fa0e2fcd22ddbe97407addf47afa
Lucas <lucas94gv@gmail.com>
2024-02-20 17:15:49 +0100

    add tests for registration

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -24,19 +24,45 @@ RSpec.describe('Users API', type: :request) do
 
       FactoryBot.create(:company) unless Company.first
 
+      total_users = User.count
+
       response '201', 'OK' do
         let(:user) do
           { user: { email: Faker::Internet.email(domain: 'gmail.com'), password: 'abc123.',
                     company_id: Company.first.id, role: 'employee' } }
         end
 
-        run_test!
+        run_test! do |response|
+          parsed_response = JSON.parse(response.body)
+          expect(User.count).to eq(total_users+1)
+          expect(parsed_response.keys.count).to(eq(7))
+          expect(parsed_response.keys).to(eq(['id', 'email', 'company_id', 'role', 'created_at', 'updated_at', 'jti']))
+          expect(parsed_response['email']).to eq(user[:user][:email])
+          expect(parsed_response['role']).to eq('employee')
+        end        
       end
 
-      response '422', 'Unprocessable entity' do
+      response '422', 'Unprocessable entity without values' do
         let(:user) { { user: { email: '', password: '', company_id: '', role: '' } } }
 
-        run_test!
+        run_test! do |response|
+          parsed_response = JSON.parse(response.body)
+          expect(User.count).to eq(total_users)
+          expect(parsed_response['errors']).to be_present
+        end
+      end
+
+      response '422', 'Unprocessable entity with repeated email' do
+
+        FactoryBot.create(:employee) unless User.first
+
+        let(:user) { { user: { email: User.first.email, password: 'abc123.', company_id: Company.first.id } } }
+
+        run_test! do |response|
+          parsed_response = JSON.parse(response.body)
+          expect(User.count).to eq(total_users)
+          expect(parsed_response['errors']).to be_present
+        end
       end
     end
   end
