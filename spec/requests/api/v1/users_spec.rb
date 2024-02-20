c2f5d4752558246e4ee054df0b7c8b45ae2d21cc
Lucas <lucas94gv@gmail.com>
2024-02-20 19:47:37 +0100

    users_spec refactor

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -34,12 +34,12 @@ RSpec.describe('Users API', type: :request) do
 
         run_test! do |response|
           parsed_response = JSON.parse(response.body)
-          expect(User.count).to eq(total_users+1)
+          expect(User.count).to eq(total_users + 1)
           expect(parsed_response.keys.count).to(eq(7))
-          expect(parsed_response.keys).to(eq(['id', 'email', 'company_id', 'role', 'created_at', 'updated_at', 'jti']))
+          expect(parsed_response.keys).to(eq(%w[id email company_id role created_at updated_at jti]))
           expect(parsed_response['email']).to eq(user[:user][:email])
           expect(parsed_response['role']).to eq('employee')
-        end        
+        end
       end
 
       response '422', 'Unprocessable entity without values' do
@@ -53,7 +53,6 @@ RSpec.describe('Users API', type: :request) do
       end
 
       response '422', 'Unprocessable entity with repeated email' do
-
         FactoryBot.create(:employee) unless User.first
 
         let(:user) { { user: { email: User.first.email, password: 'abc123.', company_id: Company.first.id } } }
@@ -83,9 +82,11 @@ RSpec.describe('Users API', type: :request) do
                 }
 
       response '200', 'OK' do
-        
-        FactoryBot.create(:employee, email: 'test@example.com', password: 'abc123.') unless User.find_by(email: 'test@example.com')
-        
+        unless User.find_by(email: 'test@example.com')
+          FactoryBot.create(:employee, email: 'test@example.com',
+                                       password: 'abc123.')
+        end
+
         before do
           User.find_by(email: 'test@example.com').confirm
         end
@@ -93,16 +94,17 @@ RSpec.describe('Users API', type: :request) do
         let(:user) do
           { user: { email: 'test@example.com', password: 'abc123.' } }
         end
-        
+
         run_test! do |response|
           parsed_response = JSON.parse(response.body)
           expect(parsed_response.keys.count).to(eq(1))
           expect(parsed_response.keys).to(eq(['token']))
           expect(parsed_response['token']).to be_kind_of(String)
-          decoded_token = JWT.decode(parsed_response['token'], ENV['devise_jwt_secret_key'], true, algorithm: 'HS256').first
+          decoded_token = JWT.decode(parsed_response['token'], ENV['devise_jwt_secret_key'], true,
+                                     algorithm: 'HS256').first
           expect(decoded_token['sub']).to eq(User.find_by(email: 'test@example.com').id)
           expect(decoded_token['jti']).to eq(User.find_by(email: 'test@example.com').jti)
-          expect(decoded_token['exp']).to be > Time.now.to_i 
+          expect(decoded_token['exp']).to be > Time.now.to_i
         end
       end
 
