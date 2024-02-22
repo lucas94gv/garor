2b1f31a12f4a35bdedc56611e8fd88d5463d398a
Lucas <lucas94gv@gmail.com>
2024-02-22 19:46:51 +0100

    add class token_authentication

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # Controller to manage operations related to companies.
     class CompaniesController < ApplicationController
+      before_action :authenticate_user!
+
       def index
         @companies = Company.all
       end
